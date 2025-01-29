b6d7301243fbd24b99fb6ad3b928482f8e3afbd4
88Simn88 <simnsanchez@yahoo.com.ar>
2025-01-29 17:03:06 -0300

    added social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,40 @@
+import { FaInstagram, FaSpotify, FaYoutube } from "react-icons/fa"
 
 const Footer = () => {
 
     return(
         <div className="bg-black text-white py-8 text-center">
-        <p className="text-sm">
+          <div className="flex gap-10 justify-center mb-4">
+           
+          <a
+                    href="https://open.spotify.com/intl-es/artist/5hC47fqjdvikID2Bxjduj4?si=hzop0Yt0RLeJJxgT2lDxww"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-lime-500 transition duration-300"
+                >
+            <FaSpotify size={50}/>
+            </a>
+
+            <a
+                    href="http://www.youtube.com/channel/UCAg584ebSCvwZEEPC_sVBrA"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-red-500 transition duration-300"
+                >
+
+            <FaYoutube size={50}/>
+            </a>
+
+            <a
+                    href="https://www.instagram.com/labestiacumbiera/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-lime-500 transition duration-300"
+                >
+            <FaInstagram size={50}/>
+            </a>
+          </div>
+        <p className="text-sm md:mx-5 mx-5">
           © {new Date().getFullYear()} La Bestia Cumbiera y La Tutuka Sonidera. Todos los derechos reservados.
         </p>
         <p className="pt-4">Coded by <a 
